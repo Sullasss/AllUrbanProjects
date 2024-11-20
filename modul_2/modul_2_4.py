6caf1364b707ab80507ac785322e1e320d2eb8e3
Sullasss <Sullasuleymanov@gmail.com>
2024-11-20 22:00:24 +0300

    Все файлы по 1 модулю и 2 дз по 2 модулю

--- modul_2/modul_2_4.py
+++ modul_2/modul_2_4.py
@@ -2,14 +2,14 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 for i in numbers:
-    if i > 1:
-        for j in range(2, i):
-            if i % j == 0:
-                not_primes.append(j)
-                break
-        else:
-            primes.append(i)
-    else:
-        not_primes.append(i)
-print(f'Primes: {primes}')
-print(f'Not primes: {not_primes}')
+    is_prime = True
+    for delitel in primes:
+        if i % delitel == 0:
+            is_prime = False
+            not_primes.append(i)
+            break
+    if is_prime == True and i != 1:
+        primes.append(i)
+
+print('Primes:', primes)
+print('Not Primes:', not_primes)

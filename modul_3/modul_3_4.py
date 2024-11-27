0290ecbb2ec8595f409b9ed679ec1beed370035a
Sullasss <Sullasuleymanov@gmail.com>
2024-11-27 15:04:01 +0300

    Все файлы по 1 модулю и 2 дз по 2 модулю

--- modul_3/modul_3_4.py
+++ modul_3/modul_3_4.py
@@ -1,43 +1,12 @@
-same_words = []
-words = []
 def single_root_words(root_word, *other_words):
     same_words = []
-    words = []
-    word = str(root_word).lower()
-    other_words = str(*other_words)
-    l = 0
-    start = 0
-    while l < len(other_words):
-        ltr = other_words[l]
-        if ltr == ' ':
-            l = l + 1
-            nxt_word = other_words[start:l - 1]
-            start = l
-            if len(nxt_word) == 0:
-                continue
-            words.append(nxt_word)
-            if nxt_word.lower() in word:
-                print(f'{root_word} содержит слово {nxt_word}')
-                same_words.append(nxt_word)
-            elif word in nxt_word.lower():
-                same_words.append(nxt_word)
-        elif l == len(other_words) - 1:
-            words.append(other_words[start:l + 1])
-            if other_words[start:l + 1].lower() in word:
-                print(f'{root_word} содержит слово {nxt_word}')
-                same_words.append(other_words[start:l + 1])
-            elif other_words[start:l + 1].lower().count(word):
-                same_words.append(other_words[start:l + 1])
-            break
-        else:
-            l = l + 1
-            continue
-    print('Проверяемые слова:', words)
-    l_other_words = ' ' + other_words.lower() + ' '
-    n = l_other_words.count(word)
-    print("Проверочное слово: ", root_word)
-    if n == 0:
-        print('Однокоренных слов не встретилось')
-    else:
-        s = l_other_words.count(' ')
+    for word in other_words:
+        if root_word.lower() in word.lower() or word.lower() in root_word.lower():
+            same_words.append(word)
     return same_words
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+
+print(result1)  # Вывод: ['richiest', 'orichalcum', 'richies']
+print(result2)  # Вывод: ['Able', 'Disable']

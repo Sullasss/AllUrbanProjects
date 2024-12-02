82c6c9d52ec21641b045d407e54ce040de200350
Sullasss <Sullasuleymanov@gmail.com>
2024-12-03 00:02:18 +0300

    Все файлы по 1 модулю и 2 дз по 2 модулю

--- modul_3/modul_3_2.py
+++ modul_3/modul_3_2.py
@@ -16,10 +16,12 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
     # Проверка на отправителя по умолчанию
     if sender == "university.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+    else:
+        print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <sender> на адрес <recipient>.")
         return
 
 # Примеры использования функции
-send_email("Привет!", "student@example.com")  # Письмо успешно отправлено с адреса university.help@gmail.com на адрес student@example.com.
-send_email("Привет!", "invalid_email")        # Невозможно отправить письмо с адреса university.help@gmail.com на адрес invalid_email
-send_email("Привет!", "student@example.com", "student@example.com")  # Нельзя отправить письмо самому себе!
-send_email("Привет!", "student@example.com", "invalid_email")
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')  # Письмо успешно отправлено с адреса university.help@gmail.com на адрес student@example.com.
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com') # Невозможно отправить письмо с адреса university.help@gmail.com на адрес invalid_email
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')  # Нельзя отправить письмо самому себе!
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')

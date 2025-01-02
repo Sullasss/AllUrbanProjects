f647a691cf5a1dcb87da1bf6a3936abceed67bb7
Sullasss <Sullasuleymanov@gmail.com>
2025-01-02 23:29:18 +0300

    Все файлы по 1 модулю и 2 дз по 2 модулю

---
+++ modul_5/1_Attributes and methods of an object.py
@@ -0,0 +1,24 @@
+class House:
+    def __init__(self, name, number_of_the_floors):
+        self.name= name
+        self.number_of_the_floors = number_of_the_floors
+
+    def go_to(self, new_floor):
+        if 1 <= new_floor <= self.number_of_the_floors:
+            print(f'Вы находитесь в здании {self.name}. Переход на этаж {new_floor}')
+            for floor in range(1, new_floor+1):
+                print(floor)
+        else:
+            print(f'Такого этажа не существует')
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+
+h1.go_to(5)
+h2.go_to(10)
+
+
+
+
+
+
